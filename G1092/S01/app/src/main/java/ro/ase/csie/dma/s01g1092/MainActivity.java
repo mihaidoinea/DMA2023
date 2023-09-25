67ecab21df65ec1840954d1244f8df58da7c2b20
Mihai Doinea <mihai.doinea@oracle.com>
2023-09-25 10:29:11 +0300

    First app 1092

---
+++ G1092/S01/app/src/main/java/ro/ase/csie/dma/s01g1092/MainActivity.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.dma.s01g1092;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
