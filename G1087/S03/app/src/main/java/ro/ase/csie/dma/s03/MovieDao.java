1ea98058cee99f66db6608029fea5fe6de0c400a
Mihai Doinea <mihai.doinea@oracle.com>
2024-01-07 20:01:45 +0200

    G1087_S12

--- G1087/S03/app/src/main/java/ro/ase/csie/dma/s03/MovieDao.java
+++ G1087/S03/app/src/main/java/ro/ase/csie/dma/s03/MovieDao.java
@@ -1,6 +1,7 @@
 package ro.ase.csie.dma.s03;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
@@ -14,5 +15,8 @@ public interface MovieDao {
 
     @Insert
     long insert(Movie movie);
+
+    @Delete
+    int delete(Movie movie);
 
 }
